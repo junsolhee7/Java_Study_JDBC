0bd3f77ec1eb21648048d6efdb9790cd948fed94
junsolhee7 <junsolhee7@naver.com>
2022-07-27 12:22:41 +0900

    1

--- src/com/iu/test/TestMain.java
+++ src/com/iu/test/TestMain.java
@@ -7,7 +7,7 @@ public class TestMain {
 	public static void main(String[] args) {
 		DBConnector dbConnector = new DBConnector();
 		try {
-			dbConnector.getConnection();
+			DBConnector.getConnection();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
